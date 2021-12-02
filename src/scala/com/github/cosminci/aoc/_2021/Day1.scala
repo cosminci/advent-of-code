2e0f57897840452c158a09fa4337213ed9b39f43
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-02 09:17:28 +0200

    [DAY1] Optimize solution for part 2

--- src/scala/com/github/cosminci/aoc/_2021/Day1.scala
+++ src/scala/com/github/cosminci/aoc/_2021/Day1.scala
@@ -12,8 +12,6 @@ object Day1 {
   def countDepthIncreases(measurements: Seq[Int]): Int =
     (1 until measurements.length).count(i => measurements(i) > measurements(i - 1))
 
-  def countWindowDepthIncreases(measurements: Seq[Int]): Int = {
-    val windowSums = measurements.sliding(3).map(_.sum).toSeq
-    (1 until windowSums.length).count(i => windowSums(i) > windowSums(i - 1))
-  }
+  def countWindowDepthIncreases(measurements: Seq[Int]): Int =
+    (3 until measurements.length).count(i => measurements(i) > measurements(i - 3))
 }

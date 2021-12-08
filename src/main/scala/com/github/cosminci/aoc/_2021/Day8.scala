8d5d3caa6a908bb68dc354854587da9ea5262181
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-09 00:22:15 +0200

    [DAY8] Rewrite part 2 more concisely

--- src/main/scala/com/github/cosminci/aoc/_2021/Day8.scala
+++ src/main/scala/com/github/cosminci/aoc/_2021/Day8.scala
@@ -18,41 +18,29 @@ object Day8 {
   def countUniqueCombinationDigits(input: Seq[(Seq[String], Seq[String])]): Int =
     input.flatMap(_._2).count(out => Set(2, 3, 4, 7).contains(out.length))
 
-  def sumOutputValues(input: Seq[(Seq[String], Seq[String])]): Int = {
-    val segmentsToDigits =
-      Seq("abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg")
-        .zip((0 to 9))
-        .toMap
+  def sumOutputValues(input: Seq[(Seq[String], Seq[String])]): Int =
+    input.flatMap { case (combinations, outputs) =>
+      deduceMappings(combinations).map { mappings =>
+        outputs
+          .map(o => mappings(o.toSet))
+          .foldLeft(0)((v, digit) => v * 10 + digit)
+      }
+    }.sum
 
-    def solve(combinations: Seq[String], outputs: Seq[String]): Int = {
-      val Some((segmentA, segmentsCF, segmentsACF, segmentsBCDF)) = for {
-        segmentsCF   <- combinations.find(_.length == 2).map(_.toSet) // One
-        segmentsACF  <- combinations.find(_.length == 3).map(_.toSet) // Seven
-        segmentsBCDF <- combinations.find(_.length == 4).map(_.toSet) // Four
-      } yield (segmentsACF.diff(segmentsCF).head, segmentsCF, segmentsACF, segmentsBCDF)
-
-      val segments   = ('a' to 'g').toSet
-      val unknown    = combinations.filterNot(c => Set(2, 3, 4, 7).contains(c.length))
-      val segmentE   = segments.filter(s => unknown.flatten.count(_ == s) == 3).head
-      val segmentsAG = segments.filter(s => unknown.flatten.count(_ == s) == 6)
-      val segmentG   = (segmentsAG - segmentA).head
-      val segmentsBC = segments.filter(s => unknown.flatten.count(_ == s) == 4)
-      val segmentC   = segmentsACF.intersect(segmentsBC).head
-      val segmentB   = (segmentsBC - segmentC).head
-      val segmentF   = (segmentsCF - segmentC).head
-      val segmentsBD = segmentsBCDF.diff(segmentsACF)
-      val segmentD   = (segmentsBD - segmentB).head
-
-      val rewiring =
-        Seq(segmentA, segmentB, segmentC, segmentD, segmentE, segmentF, segmentG)
-          .zip('a' to 'g')
-          .toMap
-
-      outputs
-        .map(out => segmentsToDigits(out.map(rewiring).sorted))
-        .foldLeft(0)((v, digit) => v * 10 + digit)
-    }
-
-    input.map { case (combinations, output) => solve(combinations, output) }.sum
-  }
+  private def deduceMappings(combinations: Seq[String]) =
+    for {
+      one   <- combinations.find(_.length == 2)
+      seven <- combinations.find(_.length == 3)
+      four  <- combinations.find(_.length == 4)
+      eight <- combinations.find(_.length == 7)
+      six   <- combinations.find(c => c.length == 6 && c.diff(one).length == 5)
+      nine  <- combinations.find(c => c.length == 6 && c.diff(four).length == 2)
+      zero  <- combinations.find(c => c.length == 6 && c != six && c != nine)
+      three <- combinations.find(c => c.length == 5 && c.diff(one).length == 3)
+      two   <- combinations.find(c => c.length == 5 && c.diff(nine).length == 1)
+      five  <- combinations.find(c => c.length == 5 && c != two && c != three)
+    } yield Seq(zero, one, two, three, four, five, six, seven, eight, nine)
+      .map(_.toSet)
+      .zip(0 to 9)
+      .toMap
 }

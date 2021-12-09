b37eb3709c22367b4bf13d249c53c9a5836b2f38
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-09 10:23:06 +0200

    [DAY9] Reimplement part II using recursive DFS

--- src/main/scala/com/github/cosminci/aoc/_2021/Day9.scala
+++ src/main/scala/com/github/cosminci/aoc/_2021/Day9.scala
@@ -17,35 +17,31 @@ object Day9 {
       r <- grid.indices
       c <- grid(r).indices
       if utils.neighbours(grid, r, c).forall { case (r1, c1) => grid(r)(c) < grid(r1)(c1) }
-    } yield (r, c)
+    } yield grid(r)(c) + 1
 
-    lowPoints.map { case (r, c) => grid(r)(c) + 1 }.sum
+    lowPoints.sum
   }
 
   def largestBasinsProduct(grid: Seq[Seq[Int]]): Int = {
     val visited = mutable.Set.empty[(Int, Int)]
-    def bfs(r: Int, c: Int): Int = {
-      visited.add((r, c))
-      val toVisit = mutable.Queue((r, c))
-      var size    = 0
-      while (toVisit.nonEmpty) {
-        val (r, c) = toVisit.dequeue()
-        size += 1
-        utils.neighbours(grid, r, c).foreach { case (r1, c1) =>
-          if (grid(r1)(c1) != 9 && !visited.contains((r1, c1))) {
+
+    def dfs(r: Int, c: Int): Int =
+      1 + utils
+        .neighbours(grid, r, c)
+        .collect {
+          case (r1, c1) if grid(r1)(c1) != 9 && !visited.contains((r1, c1)) =>
             visited.add((r1, c1))
-            toVisit.enqueue((r1, c1))
-          }
-        }
-      }
-      size
-    }
+            dfs(r1, c1)
+        }.sum
 
     val sizes = for {
       r <- grid.indices
       c <- grid(r).indices
       if grid(r)(c) != 9 && !visited.contains((r, c))
-    } yield bfs(r, c)
+    } yield {
+      visited.add((r, c))
+      dfs(r, c)
+    }
 
     sizes.sorted.takeRight(3).product
   }

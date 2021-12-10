cd5a7e45bf41562a2cbaebdce780abdffd369cdd
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-10 20:40:14 +0200

    [DAY6] Add iterative solutions for both parts

--- src/main/scala/com/github/cosminci/aoc/_2021/Day6.scala
+++ src/main/scala/com/github/cosminci/aoc/_2021/Day6.scala
@@ -7,11 +7,14 @@ import scala.collection.mutable
 object Day6 {
   def main(args: Array[String]): Unit = {
     val fish = utils.loadInputAsStrings("2021/day6.txt").head.split(',').map(_.toInt)
-    println(s"Part I: ${simulate(fish, 80)}")
-    println(s"Part II: ${simulate(fish, 256)}")
+    println(s"Part I: ${simulateTopDownDP(fish, 80)}")
+    println(s"Part II: ${simulateTopDownDP(fish, 256)}")
+
+    println(s"Part I: ${simulateSlidingTimeline(fish, 80)}")
+    println(s"Part II: ${simulateSlidingTimeline(fish, 256)}")
   }
 
-  def simulate(fish: Seq[Int], daysLeft: Int): Long = {
+  def simulateTopDownDP(fish: Seq[Int], daysLeft: Int): Long = {
     val mem = mutable.Map.empty[(Int, Int), Long]
     def dfs(delay: Int, daysLeft: Int): Long =
       mem.getOrElseUpdate((delay, daysLeft), {
@@ -20,5 +23,14 @@ object Day6 {
         else dfs(6, daysLeft - 1) + dfs(8, daysLeft - 1)
       })
     fish.map(dfs(_, daysLeft)).sum
+  }
+
+  def simulateSlidingTimeline(fish: Seq[Int], daysLeft: Int): Long = {
+    val initialSchedule = fish.foldLeft(Seq.fill(9)(0L)) { (schedule, fish) =>
+      schedule.updated(fish, schedule(fish) + 1)
+    }
+    (1 to daysLeft).foldLeft(initialSchedule) { (schedule, _) =>
+      schedule.updated(7, schedule(7) + schedule.head).tail :+ schedule.head
+    }.sum
   }
 }

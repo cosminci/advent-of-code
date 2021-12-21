ac428e87bc1debb26adee152ef6a8144da88e495
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-21 14:25:43 +0200

    [DAY9] Reimplement DFS w/o mutability

--- src/main/scala/com/github/cosminci/aoc/_2021/Day9.scala
+++ src/main/scala/com/github/cosminci/aoc/_2021/Day9.scala
@@ -2,8 +2,6 @@ package com.github.cosminci.aoc._2021
 
 import com.github.cosminci.aoc.utils
 
-import scala.collection.mutable
-
 object Day9 {
   def main(args: Array[String]): Unit = {
     val input = utils.loadInputAsStrings("2021/day9.txt").map(_.toCharArray.map(_ - '0').toSeq)
@@ -25,26 +23,23 @@ object Day9 {
 
   def largestBasinsProduct(grid: Seq[Seq[Int]]): Int = {
     val (n, m) = (grid.length - 1, grid.head.length - 1)
-    val visited = mutable.Set.empty[(Int, Int)]
 
-    def dfs(r: Int, c: Int): Int =
-      1 + utils
-        .neighbours(n, m, r, c)
-        .collect {
-          case (r1, c1) if grid(r1)(c1) != 9 && !visited.contains((r1, c1)) =>
-            visited.add((r1, c1))
-            dfs(r1, c1)
-        }.sum
+    def dfs(r: Int, c: Int, visited: Set[(Int, Int)]): (Set[(Int, Int)], Int) =
+      utils.neighbours(n, m, r, c).foldLeft(visited, 1) { case ((visited, size), (r, c)) =>
+        if (grid(r)(c) == 9 || visited.contains((r, c))) (visited, size)
+        else {
+          val (newVisited, count) = dfs(r, c, visited + ((r, c)))
+          (newVisited, size + count)
+        }
+      }
 
-    val sizes = for {
-      r <- grid.indices
-      c <- grid(r).indices
-      if grid(r)(c) != 9 && !visited.contains((r, c))
-    } yield {
-      visited.add((r, c))
-      dfs(r, c)
-    }
-
-    sizes.sorted.takeRight(3).product
+    (0 until m).flatMap(r => (0 until n).map(c => (r, c)))
+      .foldLeft(Set.empty[(Int, Int)], Seq.empty[Int]) { case ((visited, sizes), (r, c)) =>
+        if (grid(r)(c) == 9 || visited.contains((r, c))) (visited, sizes)
+        else {
+          val (newVisited, size) = dfs(r, c, visited + ((r, c)))
+          (newVisited, sizes :+ size)
+        }
+      }._2.sorted.takeRight(3).product
   }
 }

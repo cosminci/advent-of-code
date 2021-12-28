f29979224541c98c31f9e71b88d1347c65d3ec44
Cosmin Ciobanu <ciobanu@adobe.com>
2021-12-28 14:09:44 +0200

    [2020][DAY3] Port solution from original repo

---
+++ src/main/scala/com/github/cosminci/aoc/_2020/Day3.scala
@@ -0,0 +1,22 @@
+package com.github.cosminci.aoc._2020
+
+import com.github.cosminci.aoc.utils
+
+object Day3 {
+
+  def main(args: Array[String]): Unit = {
+    val grid = utils.loadInputAsStrings("2020/day3.txt")
+
+    val slopes = Seq((3, 1), (1, 1), (5, 1), (7, 1), (1, 2))
+    println(s"Part 1: ${countTreesForSlope(grid, dy = slopes.head._1, dx = slopes.head._2)}")
+    println(s"Part 2: ${slopes.map { case (dy, dx) => countTreesForSlope(grid, dy, dx) }.product }")
+  }
+
+  private def countTreesForSlope(grid: Seq[String], dy: Int, dx: Int): Long =
+    Iterator
+      .iterate(0)(_ + dx)
+      .takeWhile(_ < grid.length)
+      .zipWithIndex
+      .count { case (row, idx) => grid(row)((dy * idx) % grid.head.length) == '#' }
+
+}

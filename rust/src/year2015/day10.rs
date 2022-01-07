a10dedf07686302d2f43135f4bc85d31e8d7373e
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-07 23:27:59 +0200

    [2015][DAY10] Add solutions for both parts

---
+++ rust/src/year2015/day10.rs
@@ -0,0 +1,21 @@
+use std::borrow::Borrow;
+
+use itertools::Itertools;
+
+pub fn solve() {
+    let seed = "1113222113";
+    println!("Part 1: {}", play(seed, 40));
+    println!("Part 2: {}", play(seed, 50));
+}
+
+fn play(seed: &str, turns: u8) -> usize {
+    if turns == 0 {
+        seed.len()
+    } else {
+        let next_seed = seed.chars()
+            .group_by(|&v| v).borrow().into_iter()
+            .map(|(v, c)| format!("{}{}", c.collect_vec().len(), v))
+            .collect_vec().concat();
+        play(next_seed.as_str(), turns - 1)
+    }
+}

d10fcccd14df152c67e82da6eee9c6703fc406f3
Cosmin Ciobanu <ciobanu@adobe.com>
2022-01-12 21:04:17 +0200

    [UTILS] Add grid neighbours function

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -20,3 +20,16 @@ pub fn read_lines_as<T>(filename: impl AsRef<Path>) -> Vec<T>
 pub fn read_first_line(filename: impl AsRef<Path>) -> String {
     read_lines(filename).first().unwrap().to_string()
 }
+
+pub fn neighbours(row: usize, col: usize, rows: usize, cols: usize, diag: bool) -> Vec<(usize, usize)> {
+    let (r, c) = (row as isize, col as isize);
+    let (m, n) = (rows as isize, cols as isize);
+    let straight = vec![(r - 1, c), (r + 1, c), (r, c - 1), (r, c + 1)];
+    let diags = vec![(r - 1, c - 1), (r - 1, c + 1), (r + 1, c - 1), (r + 1, c + 1)];
+
+    let to_check = if !diag { straight } else { [straight, diags].concat() };
+    to_check.into_iter()
+        .filter(|&(r, c)| r >= 0 && c >= 0 && r < m && c < n)
+        .map(|(r, c)| (r as usize, c as usize))
+        .collect_vec()
+}

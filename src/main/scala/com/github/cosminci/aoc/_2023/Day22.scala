e66c58d77cadcbce3793f0e427b63be776591e3e
Cosmin Ciobanu <cosmin.ciobanu12@gmail.com>
2023-12-22 14:39:14 +0200

    [2023][DAY22] Add solution for part II

--- src/main/scala/com/github/cosminci/aoc/_2023/Day22.scala
+++ src/main/scala/com/github/cosminci/aoc/_2023/Day22.scala
@@ -9,47 +9,72 @@ object Day22 {
     val input  = utils.loadInputAsStrings("2023/day22.txt")
     val bricks = layBricks(parseInput(input).sortBy(_.z.head))
 
-    println(s"Part 1: ${countDisintegrableBricks(bricks)}")
+    println(s"Part 1: ${countOptionalBricks(bricks)}")
+    println(s"Part 2: ${sumBricksToFall(bricks)}")
   }
 
   final case class Brick(x: Range, y: Range, z: Range)
 
-  def countDisintegrableBricks(bricks: Seq[Brick]): Int = {
-    val bricksByEndZ   = bricks.groupBy(_.z.last).view.mapValues(_.toSet).toMap
+  def countOptionalBricks(bricks: Seq[Brick]): Int = {
+    val bricksByEndZ = bricks.groupBy(_.z.last).view.mapValues(_.toSet).toMap
+    val supporting   = computeSupporting(bricks)
+
+    supporting.count { case (brick, supported) =>
+      supported.forall { supportedBrick =>
+        (bricksByEndZ(brick.z.last) - brick).exists(blocks(_, supportedBrick))
+      }
+    }
+  }
+
+  def sumBricksToFall(bricks: Seq[Brick]): Int = {
+    val supporting  = computeSupporting(bricks)
+    val supportedBy = reverseSupporting(supporting)
+
+    @annotation.tailrec
+    def dfs(toMove: Set[Brick], moved: Set[Brick]): Int = {
+      val newMoved  = moved ++ toMove
+      val newToMove = toMove.flatMap(supporting).filter(supportedBy(_).diff(newMoved).isEmpty)
+      if (newToMove.isEmpty) newMoved.size - 1 else dfs(newToMove, newMoved)
+    }
+
+    bricks.map(b => dfs(toMove = Set(b), moved = Set.empty)).sum
+  }
+
+  private def computeSupporting(bricks: Seq[Brick]) = {
     val bricksByStartZ = bricks.groupBy(_.z.head).view.mapValues(_.toSet).toMap
 
-    def bricksSupportedOnlyBy(brick: Brick): Set[Brick] =
-      bricksByStartZ.get(brick.z.last + 1).map(_.filter(blocks(brick, _))) match {
-        case None => Set.empty
-        case Some(supportedBricks) =>
-          supportedBricks.filterNot { supportedBrick =>
-            val otherSupporting = bricksByEndZ(brick.z.last) - brick
-            otherSupporting.exists(blocks(_, supportedBrick))
-          }
-      }
+    def bricksSupportedBy(brick: Brick): Set[Brick] =
+      bricksByStartZ.getOrElse(brick.z.last + 1, Set.empty).filter(blocks(brick, _))
 
-    bricks.count(bricksSupportedOnlyBy(_).isEmpty)
+    bricks.map(b => b -> bricksSupportedBy(b)).toMap
   }
 
   private def layBricks(bricks: Seq[Brick]) =
-    bricks.foldLeft(Seq.empty[Brick]) { (stableBricks, brick) =>
-      val candidates = stableBricks.filter(blocks(_, brick))
+    bricks.foldLeft(Seq.empty[Brick]) { (settledBricks, brick) =>
+      val candidates = settledBricks.filter(blocks(_, brick))
       val maxZ       = candidates.map(_.z.max).maxOption.getOrElse(0)
-      stableBricks :+ layBrick(brick, maxZ)
+      settledBricks :+ layBrick(brick, maxZ)
     }
 
   private def layBrick(brick: Brick, baseZ: Int): Brick =
     brick.focus(_.z).modify(z => baseZ + 1 to (baseZ + z.length))
 
-  private def blocks(stable: Brick, falling: Brick): Boolean =
-    overlap(stable.x, falling.x) && overlap(stable.y, falling.y)
+  private def blocks(settled: Brick, falling: Brick): Boolean =
+    overlap(settled.x, falling.x) && overlap(settled.y, falling.y)
 
   private def overlap(r1: Range, r2: Range): Boolean =
     (r1.head <= r2.last && r1.last >= r2.head) ||
       (r2.head <= r1.last && r2.last >= r1.head)
 
-  private def parseInput(input: Seq[String]) = input.map { case s"$x1,$y1,$z1~$x2,$y2,$z2" =>
-    Brick(x1.toInt to x2.toInt, y1.toInt to y2.toInt, z1.toInt to z2.toInt)
-  }
+  private def reverseSupporting(supporting: Map[Brick, Set[Brick]]) =
+    supporting.toSeq
+      .flatMap { case (brick, supported) => supported.map(_ -> brick) }
+      .groupMap { case (supported, _) => supported } { case (_, supporting) => supporting }
+      .view.mapValues(_.toSet).toMap
+
+  private def parseInput(input: Seq[String]) =
+    input.map { case s"$x1,$y1,$z1~$x2,$y2,$z2" =>
+      Brick(x1.toInt to x2.toInt, y1.toInt to y2.toInt, z1.toInt to z2.toInt)
+    }
 
 }
